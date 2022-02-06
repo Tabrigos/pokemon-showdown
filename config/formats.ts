49a7789c6bbb6c95f5ea930b445b0839f91ce152
MK_404 <lellone.ciao@gmail.com>
2022-02-06 16:52:23 +0100

    Formati nuovi

--- config/formats.ts
+++ config/formats.ts
@@ -26,23 +26,107 @@ export const Formats: FormatList = [
 	{
 		section: "BudewInn Custom Formate",
 	},
+	
+	{
+		name: '[Gen 8] Metronome Battle',
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3632075/">Metronome Battle</a>`,
+		],
+
+		mod: 'gen8',
+		gameType: 'doubles',
+		ruleset: [
+			'Exact HP Mod', 'Obtainableperfinta', 'Team Preview', "Dynamax Clause", "leggendari", "misteriosi" ,'Adjust Level = 50', 'Picked Team Size = 4'
+		],
+		unbanlist: ["Leppa Berry",
+		],
+		banlist: [
+			'Pokestar Spirit', 'Shedinja + Sturdy', 'Battle Bond', 'Cheek Pouch', 'Cursed Body', 'Dry Skin', 'Fluffy', 'Fur Coat', 'Gorilla Tactics',
+			'Grassy Surge', 'Huge Power', 'Ice Body', 'Iron Barbs', 'Libero', 'Moody', 'Neutralizing Gas', 'Parental Bond', 'Perish Body', 'Poison Heal',
+			'Power Construct', 'Pressure', 'Protean', 'Pure Power', 'Rain Dish', 'Rough Skin', 'Sand Spit', 'Sand Stream', 'Snow Warning', 'Stamina',
+			'Volt Absorb', 'Water Absorb', 'Wonder Guard', 'Abomasite', 'Aguav Berry', 'Assault Vest', 'Berry', 'Berry Juice', 'Berserk Gene',
+			'Black Sludge', 'Enigma Berry', 'Figy Berry', 'Gold Berry', 'Iapapa Berry', 'Kangaskhanite', 'Leftovers', 'Mago Berry', 'Medichamite',
+			'Steel Memory', 'Oran Berry', 'Rocky Helmet', 'Shell Bell', 'Sitrus Berry', 'Wiki Berry', 'Harvest + Jaboca Berry', 'Harvest + Rowap Berry', "all items", "no item",
+		],
+		onValidateSet(set) {
+			const species = this.dex.species.get(set.species);
+			if (species.types.includes('Steel')) {
+				return [`${species.name} is a Steel-type, which is banned from Metronome Battle.`];
+			}
+			if (species.bst > 625) {
+				return [`${species.name} is banned.`, `(Pok\u00e9mon with a BST higher than 625 are banned)`];
+			}
+			const item = this.dex.items.get(set.item);
+			if (set.item && item.megaStone) {
+				const megaSpecies = this.dex.species.get(item.megaStone);
+				if (species.baseSpecies === item.megaEvolves && megaSpecies.bst > 625) {
+					return [
+						`${set.name || set.species}'s item ${item.name} is banned.`, `(Pok\u00e9mon with a BST higher than 625 are banned)`,
+					];
+				}
+			}
+			if (set.moves.length !== 1 || this.dex.moves.get(set.moves[0]).id !== 'metronome') {
+				return [`${set.name || set.species} has illegal moves.`, `(Pok\u00e9mon can only have one Metronome in their moveset)`];
+			}
+		},
+	},
+	
 	{
 		name: "[Gen 8] Monochrome Cup",
 		desc: "test",
 		gameType: 'doubles',
 		mod: "gen8",
-		
+	
 		ruleset: ['Standard NatDex', 'Adjust Level = 50', 'Picked Team Size = 4', "monochrome"],
 		
+		onValidateTeam(team) {
+
+    const color = [];
+    for (const set of team) {
+        let species = this.dex.species.get(set.species);
+        color.push(species.color);
+    }
+    for(var i = 0; i < color.length - 1; i++) {
+        if(color[i] !== color[i+1]) {
+            return ['i colori sono diversi'];
+        }
+    }
+
+}
 	},
 	
+	
+	
+	
+	{
+		name: "[Gen 8] Lv50 CG Co-Op! 3-3",
+		mod: "gen8",
+		gameType: 'multi',
+		searchShow: false,
+		tournamentShow: false,
+		rated: false,
+		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 6', 'Max Move Count = 24', 'Max Level = 9999', 'Picked Team Size = 3', 'Adjust Level = 50'],
+		
+		
+		
+	},
+	{
+		name: "[Gen 8] Lv50 CG Co-Op! 6-6",
+		mod: "gen8",
+		gameType: 'multi',
+		searchShow: false,
+		tournamentShow: false,
+		rated: false,
+		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 6', 'Max Move Count = 24', 'Max Level = 9999', 'Adjust Level = 50'],
+		
+		
+		
+	},	
 	{
 		name: "[Gen 8] Lv50 Custom Game Doubles",
 		
-		mod: 'gen8',
+		mod: 'gen8',				
 		gameType: 'doubles',
-		searchShow: false,
-		mod: "gen8",
 		battle: {trunc: Math.trunc},
 		// no restrictions, for serious (other than team preview)
 		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Adjust Level = 50'],
@@ -52,8 +136,6 @@ export const Formats: FormatList = [
 		name: "[Gen 8] Lv50 Custom Game",
 
 		mod: 'gen8',
-		searchShow: false,
-		debug: true,
 		battle: {trunc: Math.trunc},
 		// no restrictions, for serious (other than team preview)
 		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Adjust Level = 50'],
@@ -466,48 +548,6 @@ export const Formats: FormatList = [
 			'Rayquaza', 'Reshiram', 'Solgaleo', 'Urshifu-Base', 'Xerneas', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned',
 			'Zekrom', 'Power Construct', 'Focus Sash', 'Ally Switch', 'Final Gambit', 'Perish Song', 'Swagger',
 		],
-	},
-	{
-		name: '[Gen 8] Metronome Battle',
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3632075/">Metronome Battle</a>`,
-		],
-
-		mod: 'gen8',
-		gameType: 'doubles',
-		ruleset: [
-			'Max Team Size = 2',
-			'HP Percentage Mod', 'Cancel Mod',
-		],
-		banlist: [
-			'Pokestar Spirit', 'Shedinja + Sturdy', 'Battle Bond', 'Cheek Pouch', 'Cursed Body', 'Dry Skin', 'Fluffy', 'Fur Coat', 'Gorilla Tactics',
-			'Grassy Surge', 'Huge Power', 'Ice Body', 'Iron Barbs', 'Libero', 'Moody', 'Neutralizing Gas', 'Parental Bond', 'Perish Body', 'Poison Heal',
-			'Power Construct', 'Pressure', 'Protean', 'Pure Power', 'Rain Dish', 'Rough Skin', 'Sand Spit', 'Sand Stream', 'Snow Warning', 'Stamina',
-			'Volt Absorb', 'Water Absorb', 'Wonder Guard', 'Abomasite', 'Aguav Berry', 'Assault Vest', 'Berry', 'Berry Juice', 'Berserk Gene',
-			'Black Sludge', 'Enigma Berry', 'Figy Berry', 'Gold Berry', 'Iapapa Berry', 'Kangaskhanite', 'Leftovers', 'Mago Berry', 'Medichamite',
-			'Steel Memory', 'Oran Berry', 'Rocky Helmet', 'Shell Bell', 'Sitrus Berry', 'Wiki Berry', 'Harvest + Jaboca Berry', 'Harvest + Rowap Berry',
-		],
-		onValidateSet(set) {
-			const species = this.dex.species.get(set.species);
-			if (species.types.includes('Steel')) {
-				return [`${species.name} is a Steel-type, which is banned from Metronome Battle.`];
-			}
-			if (species.bst > 625) {
-				return [`${species.name} is banned.`, `(Pok\u00e9mon with a BST higher than 625 are banned)`];
-			}
-			const item = this.dex.items.get(set.item);
-			if (set.item && item.megaStone) {
-				const megaSpecies = this.dex.species.get(item.megaStone);
-				if (species.baseSpecies === item.megaEvolves && megaSpecies.bst > 625) {
-					return [
-						`${set.name || set.species}'s item ${item.name} is banned.`, `(Pok\u00e9mon with a BST higher than 625 are banned)`,
-					];
-				}
-			}
-			if (set.moves.length !== 1 || this.dex.moves.get(set.moves[0]).id !== 'metronome') {
-				return [`${set.name || set.species} has illegal moves.`, `(Pok\u00e9mon can only have one Metronome in their moveset)`];
-			}
-		},
 	},
 	{
 		name: "[Gen 8] Doubles Custom Game",
--- data/rulesets.ts
+++ data/rulesets.ts
@@ -188,6 +188,61 @@ export const Rulesets: {[k: string]: FormatData} = {
 			return [];
 		},
 	},
+
+	obtainableperfinta: {
+		effectType: 'ValidatorRule',
+		name: 'Obtainable',
+		desc: "Makes sure the team is possible to obtain in-game.",
+		ruleset: ['Obtainable Moves', 'Obtainable Abilities', 'Obtainable Formes', 'Obtainable Misc'],
+		banlist: ['Unreleased', 'Unobtainable', 'Nonexistent'],
+		// Mostly hardcoded in team-validator.ts
+		onValidateTeam(team, format) {
+			let kyuremCount = 0;
+			let necrozmaDMCount = 0;
+			let necrozmaDWCount = 0;
+			let calyrexCount = 0;
+			for (const set of team) {
+				if (set.species === 'Kyurem-White' || set.species === 'Kyurem-Black') {
+					if (kyuremCount > 0) {
+						return [
+							`You cannot have more than one Kyurem-Black/Kyurem-White.`,
+							`(It's untradeable and you can only make one with the DNA Splicers.)`,
+						];
+					}
+					kyuremCount++;
+				}
+				if (set.species === 'Necrozma-Dusk-Mane') {
+					if (necrozmaDMCount > 0) {
+						return [
+							`You cannot have more than one Necrozma-Dusk-Mane`,
+							`(It's untradeable and you can only make one with the N-Solarizer.)`,
+						];
+					}
+					necrozmaDMCount++;
+				}
+				if (set.species === 'Necrozma-Dawn-Wings') {
+					if (necrozmaDWCount > 0) {
+						return [
+							`You cannot have more than one Necrozma-Dawn-Wings`,
+							`(It's untradeable and you can only make one with the N-Lunarizer.)`,
+						];
+					}
+					necrozmaDWCount++;
+				}
+				if (set.species === 'Calyrex-Ice' || set.species === 'Calyrex-Shadow') {
+					if (calyrexCount > 0) {
+						return [
+							`You cannot have more than one Calyrex-Ice/Calyrex-Shadow.`,
+							`(It's untradeable and you can only make one with the Reins of Unity.)`,
+						];
+					}
+					calyrexCount++;
+				}
+			}
+			return [];
+		},
+	},
+	
 	obtainablemoves: {
 		effectType: 'ValidatorRule',
 		name: 'Obtainable Moves',
